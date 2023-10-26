ca987bce398a82e15b67976c9f634522ce1fe443
Kyle Pearson <kyle.a.pearson@jpl.nasa.gov>
2023-10-26 15:34:23 -0700

    downscaling input

---
+++ create_downscaling_input.py
@@ -0,0 +1,29 @@
+import pickle
+import argparse
+import pandas as pd
+
+if __name__ == "__main__":
+    # argparse for input filepath
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file_path', type=str, 
+                        help='path to input metrics file', 
+                        default="Data/kelp_metrics_31_36.pkl")
+    args = parser.parse_args()
+
+    # load data from disk
+    with open(args.file_path, 'rb') as f:
+        data = pickle.load(f)
+
+    # Access the latitude and longitude variables from the dataset
+    latitude = data['lat'][::1000]
+    longitude = data['lon'][::1000]
+
+    # add min and max lat/lon to list
+    latitude = list(latitude) + [latitude.min(), latitude.max()]
+    longitude = list(longitude) + [longitude.min(), longitude.max()]
+
+    # Create a DataFrame with latitude and longitude as columns
+    df = pd.DataFrame({'Latitude': latitude, 'Longitude': longitude})
+
+    # Save the DataFrame to a CSV file
+    df.to_csv(args.file_path.replace('.pkl', '.csv'), index=False)
